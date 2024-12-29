ad178f0e03b3dae2e7c015b8da386d96a2a24eae
narkolepsizm <aydinyasin25@gmail.com>
2024-12-30 02:44:09 +0300

    Remove unnecessary headers from file download response and set ETag

--- index.js
+++ index.js
@@ -641,10 +641,7 @@ app.get('/yedekle', cookieDogrula, (req, res) => {
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
     res.setHeader('Last-Modified', (new Date()).toUTCString());
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename=' + dosya);
-    res.setHeader('Content-Length', fs.statSync(dosyaYol).size);
-    res.setHeader('X-Suggested-Filename', dosya);
+    res.setHeader('ETag', '');
     res.download(dosyaYol, dosya);
   } else {
     res.status(404).json({mesaj: 'Aradığınız sayfa bulunamadı! '});
